d0ec19bf1c22c5884f8726ac98bc31e4dff3f40a
rahmanovicbenjamin07-hash <rahmanovicbenjamin07@gmail.com>
2026-05-26 12:04:13 +0200

    Fix: Border issues

--- src/components/wrappers/section-wrapper.tsx
+++ src/components/wrappers/section-wrapper.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '#/lib/utils.ts'
 
-function Section({ className, ...props }: React.ComponentProps<'section'>) {
-  return <section className={cn('py-20', className)} {...props} />
+const sectionVariants = cva('py-20 w-full max-w-[76.5rem] mx-auto', {
+  variants: {
+    border: {
+      none: '',
+      sides: 'border-x border-[var(--border)]',
+    },
+  },
+  defaultVariants: {
+    border: 'none',
+  },
+})
+
+function Section({
+  className,
+  border,
+  ...props
+}: React.ComponentProps<'section'> & VariantProps<typeof sectionVariants>) {
+  return (
+    <section
+      className={cn(sectionVariants({ border }), className)}
+      {...props}
+    />
+  )
 }
 
-export { Section }
+export { Section, sectionVariants }
