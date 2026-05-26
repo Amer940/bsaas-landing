2a2b894fb99c54b432ee6e33bf57aa1e63d65f3f
Amer <aamer.talo@gmail.com>
2026-05-26 13:10:17 +0200

    rewrite: - changed how the text and formatting is written in sections

--- src/components/sections/CTA-section.tsx
+++ src/components/sections/CTA-section.tsx
@@ -15,11 +15,11 @@ export function CTASection() {
         <Container>
           <div className="w-full flex flex-col items-center">
             <div className="flex flex-col items-center text-center mx-auto md:mb-8 mb-6">
-              <h2 className="md:text-50-medium text-[36px] leading-[110%] font-medium text-white">
+              <h2 className="md:text-[50px] text-[36px] leading-[110%] font-medium text-white">
                 Take care of your <br />
                 client today!
               </h2>
-              <p className="md:text-18-medium text-[14px] font-normal text-white mt-4 md:max-w-165 max-w-66.25">
+              <p className="md:text-[18px] text-[14px] font-normal text-white mt-4 md:max-w-165 max-w-66.25">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been.
               </p>
--- src/components/sections/faq-sections.tsx
+++ src/components/sections/faq-sections.tsx
@@ -37,10 +37,10 @@ export function FAQSection() {
         <Container>
           <div className="w-full flex-col">
             <div className="flex flex-col items-center text-center mx-auto md:mb-16 mb-8">
-              <h2 className="md:text-50-medium text-[36px] leading-[110%] font-medium">
+              <h2 className="md:text-[50px] text-[36px] leading-[110%] font-medium">
                 Questions, <span className="text-primary">answered</span>.
               </h2>
-              <p className="md:text-18-medium text-[14px] font-normal text-muted-foreground mt-5 md:max-w-157 max-w-71">
+              <p className="md:text-[18px] text-[14px] font-normal text-muted-foreground mt-5 md:max-w-157 max-w-71">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been.
               </p>
