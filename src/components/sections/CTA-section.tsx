f97f0db363840326fe8f7c32b333afe424a74404
rahmanovicbenjamin07-hash <rahmanovicbenjamin07@gmail.com>
2026-05-26 12:54:22 +0200

    CTA Section

---
+++ src/components/sections/CTA-section.tsx
@@ -0,0 +1,31 @@
+import { Container } from '../container'
+import { EmailInput } from '../ui/input'
+import { Section } from '../wrappers/section-wrapper'
+import image from '../../images/CTA-background-image.png'
+
+export function CTASection() {
+  return (
+    <Section
+      padding="small"
+      className="bg-white bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${image})` }}
+      border="sides"
+    >
+      <Container>
+        <div className="w-full flex flex-col items-center">
+          <div className="flex flex-col items-center text-center mx-auto md:mb-8 mb-6">
+            <h2 className="md:text-50-medium text-[36px] leading-[110%] font-medium text-white">
+              Take care of your <br />
+              client today!
+            </h2>
+            <p className="md:text-18-medium text-[14px] font-normal text-white mt-4 md:max-w-165 max-w-66.25">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been.
+            </p>
+          </div>
+          <EmailInput />
+        </div>
+      </Container>
+    </Section>
+  )
+}
