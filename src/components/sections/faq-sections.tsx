868b450653d4dce74ad27447f46b830b3e56a56f
Amer <aamer.talo@gmail.com>
2026-05-30 12:49:21 +0200

    bugfix: - cta and faq borders, spacings

--- src/components/sections/faq-sections.tsx
+++ src/components/sections/faq-sections.tsx
@@ -32,13 +32,13 @@ export function FAQSection() {
   return (
     <div className="md:px-4 px-2">
       <div className="w-full bg-white">
-        <div className="site-container site-border py-20">
+        <div className="site-container site-border py-10 md:py-20">
           <div className="w-full flex-col">
             <div className="flex flex-col items-center text-center mx-auto md:mb-16 mb-8">
               <h2 className="md:text-[50px] text-[36px] leading-[110%] font-medium">
                 Questions, <span className="text-primary">answered</span>.
               </h2>
-              <p className="md:text-[18px] text-[14px] font-normal text-muted-foreground mt-5 md:max-w-157 max-w-71">
+              <p className="md:text-[18px] text-[14px] font-normal text-muted-foreground mt-2 md:mt-5 md:max-w-157 max-w-71">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been.
               </p>
